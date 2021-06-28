2ea1a4d0d13e42f4f2b6c3e35b3b09ff04aac878
fftl <fftl.0710@gmail.com>
2021-06-28 23:09:52 +0900

    back : init

---
+++ fftl-03-RemakeBoard/src/main/java/fftl/fftl03RemakeBoard/Fftl03RemakeBoardApplication.java
@@ -0,0 +1,13 @@
+package fftl.fftl03RemakeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fftl03RemakeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fftl03RemakeBoardApplication.class, args);
+	}
+
+}
---
+++ fftl-03-RemakeBoard/src/test/java/fftl/fftl03RemakeBoard/Fftl03RemakeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package fftl.fftl03RemakeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fftl03RemakeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
