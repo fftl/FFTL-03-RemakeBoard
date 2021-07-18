8c993cb3c0661087fc6662f7ca8b8884f7dec839
fftl <fftl.0710@gmail.com>
2021-07-18 21:42:40 +0900

    front : styled-components install

--- fftl-03-front/src/App.js
+++ fftl-03-front/src/App.js
@@ -1,14 +1,29 @@
-import './App.css';
-import Body from './components/body';
-import Footer from './components/Footer';
-import Header from './components/header';
+import React from 'react';
+import styled from 'styled-components';
+
+const BaseBox = styled.div`
+    width: 100px;
+    height: 100px;
+`;
+
+const RedBox = styled(BaseBox)`
+    background-color: #ff0032;
+`;
+
+const BlueBox = styled(BaseBox)`
+    background-color: #00eaff;
+`;
+
+const GreenBox = styled(BaseBox)`
+    background-color: green;
+`;
 
 function App() {
     return (
         <div>
-            <Header />
-            <Body />
-            <Footer />
+            <RedBox />
+            <BlueBox />
+            <GreenBox />
         </div>
     );
 }
