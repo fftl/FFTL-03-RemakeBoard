c5ee39442456ef3ffa6f7c51754c775ec384ae23
fftl <fftl.0710@gmail.com>
2021-08-19 00:25:44 +0900

    back: update BoardController.java

--- fftl-03-RemakeBoard/src/main/java/fftl/fftl03RemakeBoard/controller/BoardController.java
+++ fftl-03-RemakeBoard/src/main/java/fftl/fftl03RemakeBoard/controller/BoardController.java
@@ -2,8 +2,13 @@ package fftl.fftl03RemakeBoard.controller;
 
 import fftl.fftl03RemakeBoard.entity.Board;
 import fftl.fftl03RemakeBoard.request.SaveBoardDto;
+import fftl.fftl03RemakeBoard.response.DefaultResponse;
+import fftl.fftl03RemakeBoard.response.ResponseMessage;
+import fftl.fftl03RemakeBoard.response.StatusCode;
 import fftl.fftl03RemakeBoard.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,43 +22,43 @@ public class BoardController {
 
     /** 게시글 작성 입니다. */
     @PostMapping(value="")
-    public String saveBoard(@RequestBody SaveBoardDto saveBoardDto){
+    public ResponseEntity saveBoard(@RequestBody SaveBoardDto saveBoardDto){
         Long boardId = boardService.saveBoard(saveBoardDto);
 
-        return boardId.toString();
+        return new ResponseEntity(DefaultResponse.response(StatusCode.OK, ResponseMessage.CREATED_USER), HttpStatus.OK);
     }
 
     /** 게시글 조회 입니다. */
     @GetMapping(value="/{boardId}")
-    public String findOneBoard(@PathVariable Long boardId){
+    public ResponseEntity findOneBoard(@PathVariable Long boardId){
         Board board = boardService.findByBoardId(boardId);
 
-        return board.toString();
+        return new ResponseEntity(DefaultResponse.response(StatusCode.OK, ResponseMessage.GET_ONE_BOARD, board), HttpStatus.OK);
     }
 
     /** 게시글 전체 조회 입니다. */
     @GetMapping(value="")
-    public List<Board> findAllBoard(){
+    public ResponseEntity findAllBoard(){
         List<Board> boards = boardService.findAllBoard();
 
-        return boards;
+        return new ResponseEntity(DefaultResponse.response(StatusCode.OK, ResponseMessage.GET_ALL_BOARD, boards), HttpStatus.OK);
     }
 
     /** 게시글 수정 입니다. */
     @PatchMapping(value="/{boardId}")
-    public String updateBoard(@PathVariable Long boardId, @RequestBody SaveBoardDto saveBoardDto){
+    public ResponseEntity updateBoard(@PathVariable Long boardId, @RequestBody SaveBoardDto saveBoardDto){
         Board board = boardService.findByBoardId(boardId);
         Board updatedBoard = boardService.updateBoard(board, saveBoardDto);
 
-        return  updatedBoard.toString();
+        return new ResponseEntity(DefaultResponse.response(StatusCode.OK, ResponseMessage.UPDATE_BOARD, updatedBoard), HttpStatus.OK);
     }
 
     /** 게시글 삭제 입니다. */
     @DeleteMapping(value="/{boardId}")
-    public String deleteBoard(@PathVariable Long boardId){
+    public ResponseEntity deleteBoard(@PathVariable Long boardId){
         Board board = boardService.findByBoardId(boardId);
         boardService.deleteBoard(board);
 
-        return "success delete";
+        return new ResponseEntity(DefaultResponse.response(StatusCode.OK, ResponseMessage.DELETE_BOARD), HttpStatus.OK);
     }
 }
